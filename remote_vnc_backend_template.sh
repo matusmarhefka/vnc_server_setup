77e34c1ede0b09a875e62f52ac565622df2fc703
Marek Haicman <mhaicman@redhat.com>
2017-06-24 00:45:14 +0200

    Simplification of VNC password setting
    - by creating password manually, we can create short or empty passwords
    - on rhel7, service is prepared as recommended
    - user has home dir created

--- remote_vnc_backend_template.sh
+++ remote_vnc_backend_template.sh
@@ -7,7 +7,7 @@ DISTRO_VERSION=$(grep -o "[0-9]*" /etc/redhat-release | head -1)
 
 grep -q <USERNAME> /etc/passwd
 if [ $? -ne 0 ]; then
-	useradd <USERNAME>
+	useradd -m <USERNAME>
 fi
 
 
@@ -18,32 +18,28 @@ if [ $DISTRO_VERSION = "6" ]; then
 	echo "VNCSERVERS=\"1:<USERNAME>\"
 	VNCSERVERARGS[1]=\"-geometry 1024x768\"" >> /etc/sysconfig/vncservers
 
-	/sbin/service vncserver start
-	/sbin/service vncserver stop
 	/sbin/chkconfig vncserver on
 	/sbin/service iptables stop
 
-	su - <USERNAME> -c "printf \"<PASSWD>\n<PASSWD>\n\" | vncpasswd"
+	# to force any password, we create it manually
+	su - <USERNAME> -c "mkdir -p ~/.vnc && echo \"<PASSWD>\" | vncpasswd -f > ~/.vnc/passwd && chmod 600 ~/.vnc/passwd"
 
 	/sbin/service vncserver start
-	/sbin/service vncserver stop
 
-	su - <USERNAME> -c "printf \"<PASSWD>\n<PASSWD>\n\n\" | vncpasswd"
 
-	/sbin/service vncserver start
-	echo -e "\n\nConnect to vnc server using '<REMOTE_MACHINE_IP>:1' and password '<PASSWD>'."
 elif [ $DISTRO_VERSION = "7" ]; then
 	yum groupinstall -y 'Server with GUI'
 	yum install -y tigervnc-server
 
-	sed -i "s|<USER>|<USERNAME>|g" /lib/systemd/system/vncserver@.service
+	cp /{lib,etc}/systemd/system/vncserver@.service
+	sed -i "s|<USER>|<USERNAME>|g" /etc/systemd/system/vncserver@.service
 	systemctl daemon-reload
 	systemctl enable vncserver@:1.service
-
-	su - <USERNAME> -c "printf \"<PASSWD>\n<PASSWD>\n\" | vncpasswd"
+	# to force any password, we create it manually
+	su - <USERNAME> -c "mkdir -p ~/.vnc && echo \"<PASSWD>\" | vncpasswd -f > ~/.vnc/passwd && chmod 600 ~/.vnc/passwd"
 
 	systemctl start vncserver@:1.service
-	echo -e "\n\nConnect to vnc server using '<REMOTE_MACHINE_IP>:1' and password '<PASSWD>'."
 else
 	echo "Error: Unknown distro: '$DISTRO_VERSION'" 1>&2
 fi
+echo -e "\n\nConnect to vnc server using '<REMOTE_MACHINE_IP>:1' and password '<PASSWD>'."
